9014f765bf768ed4fc737a42661a3e3a7d72275b
Angel <angel.gonzalez@xilon.es>
2020-11-25 14:25:35 +0100

    DEV-padding

--- js/core.js
+++ js/core.js
@@ -87,8 +87,7 @@ window.onload = function() {
         // y añade contenido
         var newDiv = document.createElement("div");
         var newContent = document.createTextNode(nameValue+ " es capaz de zurrarle a Chuck Norris.");
-        newContent.classList.add("p-5");
-        newDiv.classList.add("js-video-text");
+        newDiv.classList.add("js-video-text p-5");
         newDiv.appendChild(newContent); //añade texto al div creado.
         videoContainer.prepend(newDiv);
       };

420b465b75b9b474bac4242dbbef35478caa8a25
Angel <angel.gonzalez@xilon.es>
2020-11-29 14:36:14 +0100

    DEV-btn

--- js/core.js
+++ js/core.js
@@ -137,7 +137,10 @@ window.onload = function() {
     videoContainer.classList.add("active");
 
     var video = videoContainer.getElementsByTagName("video")[0];
+    if(!isVolume){
+      video.volume=0;
 
+    }
     video.play();
     video.loop = false;
 

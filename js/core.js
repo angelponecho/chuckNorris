753d5cee2539e6755007a43992ddc1904b569a7e
Angel <angel.gonzalez@xilon.es>
2020-11-30 08:42:00 +0100

    DEV-name

--- js/core.js
+++ js/core.js
@@ -95,7 +95,8 @@ window.onload = function() {
             //NO esta en la listaponecmos texto
             // crea un nuevo div// y añade contenido
             var newDiv = document.createElement("div");
-            var newContent = document.createTextNode(nameValue+ "es fan de la Pantoja y Paquirrín.");
+            var formatName = nameValue.substring(0, 1).toUpperCase() + nameValue.substring(1);
+            var newContent = document.createTextNode(formatName+ " es fan de la Pantoja y Paquirr&iacute;n.");
             newDiv.classList.add("js-video-text");
             newDiv.classList.add("p-5");
             newDiv.appendChild(newContent); //añade texto al div creado.
